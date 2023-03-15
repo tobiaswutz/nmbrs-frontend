6e901889a62b8dfc96f00f245410044dfe46909b
Tobias <tobiaswutz22@gmail.com>
2023-03-15 20:01:23 +0100

    clean

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,16 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// export const environment = {
+//   production: false,
+//   serverUrl: 'http://localhost:8080/'
+// };
+
 export const environment = {
-  production: false,
-  serverUrl: 'http://localhost:8080/'
+  production: true,
+  serverUrl: 'https://nest-server-production.up.railway.app/'
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
